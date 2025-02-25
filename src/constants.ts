3957f110007232356a4500495303e54d02c8c2c0
diegomarzaa <al426641@uji.es>
2025-02-26 00:06:51 +0100

    indentations

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 /** --------------------------------
- *  CONSTANTS & DEFAULT SETTINGS
+ *    CONSTANTS & DEFAULT SETTINGS
  * -------------------------------- */
 
 import type { PinnedEmojiSettings } from './types';
@@ -13,11 +13,11 @@ export const DEFAULT_PIN_EMOJI = '📌';
  * Default settings for the plugin
  */
 export const DEFAULT_SETTINGS: PinnedEmojiSettings = {
-  pinnedTabSize: 33,
-  labelEmojiMap: [
-    { label: 'Home',  emoji: '🏠' },
-    { label: 'Tasks', emoji: '📋' },
-    { label: 'Books', emoji: '📖' },
-  ],
+    pinnedTabSize: 33,
+    labelEmojiMap: [
+        { label: 'Home',    emoji: '🏠' },
+        { label: 'Tasks', emoji: '📋' },
+        { label: 'Books', emoji: '📖' },
+    ],
 };
 
